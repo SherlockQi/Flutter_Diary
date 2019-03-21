e048eddf3afadc6df1519f1ff7ad1eb8b2b68a1f
sherlockqi <xiaqi90@163.com>
2019-03-21 19:42:26 +0800

    update

--- image_demo/lib/main.dart
+++ image_demo/lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() =>
+    runApp(MyApp(
 
+        items: new List<String>.generate(1000, (i) => "Item $i")
+    ));
+
+/* 图片
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -23,6 +28,88 @@ class MyApp extends StatelessWidget {
             height: 200,
             color: Colors.lightBlue,
           ),
+        ),
+      ),
+    );
+  }
+}
+*/
+/* List
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Text widget',
+      home: Scaffold(
+        appBar: new AppBar(
+          backgroundColor: Colors.lightBlue,   title: new Text("ListView Demo"),
+        ),
+        body: Center(
+          child: Container(
+            height: 200.0,
+            child: new myTableView(),
+          ),
+        )
+        ),
+      );
+  }
+}
+
+// ignore: camel_case_types
+class myTableView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return ListView(
+      scrollDirection: Axis.horizontal,
+      children: <Widget>[
+        new Container(
+          width: 180.0,
+          color: Colors.red,
+        ),  new Container(
+          width: 180.0,
+          color: Colors.blueAccent,
+        ),  new Container(
+          width: 180.0,
+          color: Colors.greenAccent,
+        ),  new Container(
+          width: 180.0,
+          color: Colors.red,
+        ),  new Container(
+          width: 180.0,
+          color: Colors.deepOrange,
+        ),
+      ],
+    );
+  }
+}
+*/
+
+
+class MyApp extends StatelessWidget {
+
+  final List<String> items;
+
+  MyApp({Key key, @required this.items}) :super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Text widget',
+      home: Scaffold(
+        appBar: new AppBar(
+          backgroundColor: Colors.red, title: new Text("ListView Demo"),
+        ),
+        body:
+        new ListView.builder(
+          itemCount: 1000,
+          itemBuilder: (context, index) {
+            return new ListTile(
+              subtitle: Image.network('https://avatars3.githubusercontent.com/u/18034763?s=40&v=4'),
+              title: new Text('${items[index]}'),
+
+            );
+          },
         ),
       ),
     );
